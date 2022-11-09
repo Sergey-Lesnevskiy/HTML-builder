eacd4b3a460e332707ccf21ac2bc4bb5483deba7
Lesnevsky-Sergey <sergeybaian@mail.ru>
2022-11-09 08:14:08 +0300

    refactor:delete function test in 2 task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,48 +1,4 @@
-/* eslint-disable indent */
-// const fs = require('fs');
-// const path = require('path');
-// fs.writeFile(path.join(__dirname, 'text.txt'),'',(err) => {
-//   if (err) throw err;
-// });
-// function writeText(text){
-//     fs.appendFile(
-//         path.join(__dirname, 'text.txt'),
-//         text,
-//         err => {
-//             if (err) throw err;
-//         }
-//     );
-// }
-// function close(){
-//     const readableStream = fs.createReadStream(path.join(__dirname, 'text.txt'), {encoding: 'utf-8'});
-//     readableStream.on('data', chunk => console.log('your enter text: ',chunk, ' Goodbye...'));
-// }
-// let readline = require('readline');
 
-// let rl = readline.createInterface(process.stdin, process.stdout);
-
-// rl.question('Hello, can you write something?\n', function(answer){
-//     writeText(answer);
-//     if (answer ==='exit'){
-//         rl.close();
-//     }
-//     rl.on('line', (input) => {
-//         writeText(input);
-//         if (input ==='exit'){
-//             rl.close();
-//         }
-//       });
-// });
-// rl.on('close',function(){
-//     close();
-// });
-// process.on('exit',()=>{
-//     console.log('you have not entered a single word');
-// });
-// process.on('beforeExit', () => {
-//    close();
-//     process.exit();
-//   });
 
 const fs = require('fs');
 const path = require('path');
@@ -52,14 +8,25 @@ function writeText(text) {
     if (err) throw err;
   });
 }
+// function test(chunk){
+//   console.log(chunk);
+//  if(chunk===''){
+//   console.log(' Goodbye...');
+//  }else{
+//   console.log('your enter text: ', chunk, ' Goodbye...');
+//  }
+// }
 function close() {
-  const readableStream = fs.createReadStream(path.join(__dirname, 'text.txt'), {
-    encoding: 'utf-8',
-  });
+  // const readableStream = fs.createReadStream(path.join(__dirname, 'text.txt'), {
+  //   encoding: 'utf-8',
+  // });
  
-  readableStream.on('data', (chunk) =>
-    console.log('your enter text: ', chunk, ' Goodbye...')
-  );
+  // readableStream.on('data', (chunk) =>test(chunk)
+ 
+  //  );
+  // console.log('\n');
+  console.log(' Goodbye...');
+
 }
 fs.writeFile(path.join(__dirname, 'text.txt'), '', (err) => {
   if (err) {
@@ -81,11 +48,12 @@ fs.writeFile(path.join(__dirname, 'text.txt'), '', (err) => {
     });
     rl.on('close', function () {
       close();
-    });
-    process.on('beforeExit', () => {
-      close();
       process.exit();
     });
+    // process.on('beforeExit', () => {
+    //   close();
+    //   process.exit();
+    // });
   }
 });
 
